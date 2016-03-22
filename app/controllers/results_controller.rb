bf221f09cccc38e74a89df11e0405f5b8b5f5dcd
TinekeWillekes <tinekewillekes@hotmail.com>
2016-03-22 19:46:06 +0100

    updated resultscontroller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,5 @@
 class ResultsController < ApplicationController
   def index
-    @results = Result.select([:id, :player_id]).group('player_id')
+    @results = Result.select(:player_id).group('player_id')
   end
 end
