e726722aae85a1d880cad855cb88db20934dfa56
TinekeWillekes <tinekewillekes@hotmail.com>
2016-04-19 10:38:01 +0200

    updated table sorting for ranking

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,6 +3,7 @@ class ResultsController < ApplicationController
   
   def index
     @results = Result.select(:player_id).group('player_id').select('player_id, sum(game_points) as total').order('total desc')
+                                                           .select('player_id, count(game_points == 3) as played').order('played asc')
   end
   
   def ranking_order
