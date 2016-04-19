ff5c370787fdfde76f8077dacf3201c5cc31e6fd
TinekeWillekes <tinekewillekes@hotmail.com>
2016-04-19 10:54:22 +0200

    test updated ranking

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,7 @@ class ResultsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @results = Result.select(:player_id).group('player_id').select('sum(game_points) as total, count(game_points == 3) as played').order('total desc, played asc')
+    @results = Result.select(:player_id).group('player_id').select('sum(game_points) as total, count(player_id) as played').order('total desc, played asc')
   end
   
   def ranking_order
