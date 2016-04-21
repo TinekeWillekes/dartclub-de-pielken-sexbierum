5f5796b18d30c47b4536e5e90a63dd828f44abfa
TinekeWillekes <tinekewillekes@hotmail.com>
2016-04-21 13:24:55 +0200

    added avater foto css and progress bar

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,10 +2,10 @@ class Player < ActiveRecord::Base
   has_many :results
   has_many :games, :through => :results
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing/missing.jpg"
   validates_attachment_content_type :avatar, 
-                                    :content_type => /^image\/(png|gif|jpeg)/,
-                                    :message => 'only (png/gif/jpeg) images'
+                                    :content_type => /^image\/(png|gif|jpeg|jpg)/,
+                                    :message => 'only (png/gif/jpeg/jpg) images'
   
   validates :name, presence: true
   
