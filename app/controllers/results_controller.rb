a06d59377f32aa65ad9f97e4498b931423024067
TinekeWillekes <tinekewillekes@hotmail.com>
2016-05-22 11:21:53 +0200

    updated ranking page order legs_pro

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,6 +2,6 @@ class ResultsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @results = Result.select(:player_id).group('player_id').select('sum(game_points) as total, count(player_id) as played').order('total desc, played asc')
+    @results = Result.select(:player_id).group('player_id').select('sum(game_points) as total, count(player_id) as played, sum(legs_pro) as total_legs_pro').order('total desc, played asc, total_legs_pro desc')
   end
 end
