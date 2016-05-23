0ee375100e4d1daf4fb84f16a1173074f7eca422
TinekeWillekes <tinekewillekes@hotmail.com>
2016-05-23 16:43:40 +0200

    updated order player chart

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ActiveRecord::Base
   has_many :results
-  has_many :games, :through => :results
-  
+  has_many :games, -> { order(:game_date => :asc) }, :through => :results
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing/missing.jpg"
   validates_attachment_content_type :avatar, 
                                     :content_type => /^image\/(png|gif|jpeg|jpg)/,
@@ -15,5 +15,9 @@ class Player < ActiveRecord::Base
   
   def count_games_lost
     Game.joins(:players).where.not(:results => {:legs_pro => 4}).where(:results => {:player_id => self.id}).count
+  end
+  
+  def players_chart
+    Game.joins(:players).where(:results => {:player_id => self.id})
   end
 end
